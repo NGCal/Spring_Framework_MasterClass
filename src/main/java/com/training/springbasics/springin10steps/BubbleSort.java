5c7fa9c7254b7a077fd40ad708645b7b21fc134b
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-15 17:20:26 -0400

    Identified which of the two SortAlgorithm Beans should be used first by Spring when BinarySearch is instantiated

--- src/main/java/com/training/springbasics/springin10steps/BubbleSort.java
+++ src/main/java/com/training/springbasics/springin10steps/BubbleSort.java
@@ -1,8 +1,10 @@
 package com.training.springbasics.springin10steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSort implements SortAlgorithm {
     public int[] sort(int[] listOfNumbers) {
         //Sorting Magic
