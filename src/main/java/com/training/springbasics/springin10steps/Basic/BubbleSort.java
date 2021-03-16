67cb92c6dd4f7cdcf9896fe58a9fb2a5fba0f3b6
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-16 18:29:20 -0400

    Playing around with @PostConstruct and @preDestroy annotations

--- src/main/java/com/training/springbasics/springin10steps/Basic/BubbleSort.java
+++ src/main/java/com/training/springbasics/springin10steps/Basic/BubbleSort.java
@@ -1,13 +1,25 @@
 package com.training.springbasics.springin10steps.Basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
 
 @Component
 @Primary
 public class BubbleSort implements SortAlgorithm {
+
+    Logger LOG = LoggerFactory.getLogger(BubbleSort.class);
+
     public int[] sort(int[] listOfNumbers) {
         //Sorting Magic
         return listOfNumbers;
+    }
+
+    @PreDestroy
+    public void goodbyeMessage() {
+        LOG.info("The BubbleSort Bean will be destroy");
     }
 }
--- src/main/java/com/training/springbasics/springin10steps/Basic/QuickSort.java
+++ src/main/java/com/training/springbasics/springin10steps/Basic/QuickSort.java
@@ -1,9 +1,17 @@
 package com.training.springbasics.springin10steps.Basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component("Quick")
 public class QuickSort implements SortAlgorithm {
+
+    Logger LOG = LoggerFactory.getLogger(QuickSort.class);
+
     public int[] sort(int[] listOfNumbers) {
         //Sorting Magic
         return listOfNumbers;
@@ -12,5 +20,15 @@ public class QuickSort implements SortAlgorithm {
     public int[] sort2(int[] listOfNumbers) {
         //Sorting Magic
         return listOfNumbers;
+    }
+
+    @PostConstruct
+    public void welcomeLog() {
+        LOG.info("The QuickSort Bean has been created");
+    }
+
+    @PreDestroy
+    public void goodbyeMessage() {
+        LOG.info("The QuickSort bean will be destroy");
     }
 }
